8096ca6efcc7dbb8a5695e1ae35e7b0aa9cf0161
you@example.com <you@example.com>
2024-06-25 20:28:24 +0300

    hope i fixed something...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import { MdMenu } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 
 const Navbar = ({ setClub }) => {
-  const [nav, setNav] = useState(true);
-  const [darkMode, setDarkMode] = useState(false);
+  const [nav, setNav] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
@@ -22,49 +21,38 @@ const Navbar = ({ setClub }) => {
     if (!nav) setNav(!nav);
   };
 
-  const navStyle = {
-    backgroundColor: "#000",
-  };
-
   return (
-    <div
-      className="text-black flex justify-between items-center w-full px-4 h-35 relative z-10"
-      style={navStyle}
-    >
+    <div className="text-black flex justify-between items-center w-full px-4 h-20 relative z-10 bg-black">
       <div className="flex items-center">
-      
-        <div className="w-[300px] mr-2">
+        <div className="w-32 h-auto mr-2">
           <img
             src={require("./../images/record.png")}
             alt=""
-            className="w-auto h-auto max-h-24"
+            className="w-full h-auto max-h-24"
           />
         </div>
       </div>
       <ul className="hidden md:flex">
         <li
-          className="p-4 text-xl text-white bold"
+          className="p-4 text-xl text-white font-bold cursor-pointer"
           onClick={() => scrollToSection("about")}
         >
           О нас
         </li>
         <li
-          className="p-4 text-xl text-white bold"
+          className="p-4 text-xl text-white font-bold cursor-pointer"
           onClick={() => scrollToSection("studios")}
         >
           Студии
         </li>
         <li
-          className="p-4 text-xl text-white bold"
+          className="p-4 text-xl text-white font-bold cursor-pointer"
           onClick={() => scrollToSection("where")}
         >
           Как найти
         </li>
       </ul>
       <div className="flex items-center">
-        <label className="switch">
-          <span className="slider round"></span>
-        </label>
         <select
           onChange={(e) => changeClubHere(e.target.value)}
           className="ml-4 p-2 bg-gray-800 text-white rounded-md border border-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600"
@@ -73,41 +61,30 @@ const Navbar = ({ setClub }) => {
           <option value="Прометей">Прометей</option>
           <option value="Двор">Двор</option>
         </select>
-        <div onClick={handleNav} className="block md:hidden">
-          {!nav ? <IoMdClose size={40} /> : <MdMenu size={40} />}
+        <div onClick={handleNav} className="block md:hidden ml-4">
+          {!nav ? <MdMenu size={30} /> : <IoMdClose size={30} />}
         </div>
       </div>
       <div
-        className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] h-full bg-[#000300] border-r-gray-900 ease-in-out duration-500 z-20"
-            : "fixed left-[-100%]"
-        }
+        className={`${
+          nav ? "fixed" : "hidden"
+        } left-0 top-0 w-full h-full bg-black bg-opacity-90 z-20 flex flex-col items-center justify-center md:hidden`}
       >
-        <div className="w-[300px] mr-2">
+        <div className="w-32 h-auto mb-8">
           <img
-            src={require("./../images/record.jpg")}
+            src={require("./../images/record.png")}
             alt=""
-            className="w-auto h-auto max-h-24"
+            className="w-full h-auto max-h-24"
           />
         </div>
-        <ul className="pt-24 uppercase p-7">
-          <li
-            className="p-4 text-xl text-white bold border-b border-gray-600"
-            onClick={() => scrollToSection("about")}
-          >
+        <ul className="uppercase text-center text-white text-xl font-bold space-y-4">
+          <li onClick={() => scrollToSection("about")} className="cursor-pointer">
             О нас
           </li>
-          <li
-            className="p-4 text-xl text-white bold border-b border-gray-600"
-            onClick={() => scrollToSection("studios")}
-          >
+          <li onClick={() => scrollToSection("studios")} className="cursor-pointer">
             Студии
           </li>
-          <li
-            className="p-4 text-xl text-white bold border-b border-gray-600"
-            onClick={() => scrollToSection("where")}
-          >
+          <li onClick={() => scrollToSection("where")} className="cursor-pointer">
             Как найти
           </li>
         </ul>
